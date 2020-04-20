183e4b83400c2907227b0e4eca4136184b5f1d33
oz <ozel@post.bgu.ac.il>
2020-04-20 19:01:16 +0300

    readme now contains info about the tests

--- README.md
+++ README.md
@@ -18,3 +18,10 @@ In order to run the tests, run:
 ```
 npm test
 ```
+## Test Cases
+The `tests.ts` file includes a few simple tests, and some basic usage examples.  
+It creates 6 BoundingBoxes, and creates an array containing said BoundingBoxes.  
+Than it goes on to check the relations between some of the boxes and creates an array of separate boxes from the original array.  
+The `tests.ts` file demonstrate how to use the optional `inside` argument. By passing one of the `boxRelation` enum items, one can decide how to treat a box inside another box (default is to treat the boxes as separate).  
+That functionality is tested by checking the relation between a box inside another box twice, once with the parameter set to `inside` and once without passing the parameter at all.  
+The array is also tested twice, when the two boxes are not considered separate, one of the boxes is removed.
--- tests.ts
+++ tests.ts
@@ -14,8 +14,10 @@ const boxB: BoundingBox = {left: 20, right: 50, top: 50, bottom: 20};
 const boxC: BoundingBox = {left: 70, right: 90, top: 90, bottom: 70};
 const boxD: BoundingBox = {left: 100.9, right: 200.9, top: 200.9, bottom: 100.9};
 const boxE: BoundingBox = {left: 150, right: 190.5, top: 190.5, bottom: 150.5};
+const boxF: BoundingBox = {left: 40, right: 30, top: 30, bottom: 10};
 const arr: BoundingBox[] = [boxA, boxB, boxC, boxD, boxE, boxB, boxD];
 
+console.log("boxA and boxF: " + checkRelation(boxA, boxF) + " - " + test(checkRelation(boxA, boxF), boxRelation.invalidHorizontal));
 console.log("boxA and boxB: " + checkRelation(boxA, boxB) + " - " + test(checkRelation(boxA, boxB), boxRelation.intersect));
 console.log("boxA and boxC: " + checkRelation(boxA, boxC) + " - " + test(checkRelation(boxA, boxC), boxRelation.separate));
 console.log("boxD and boxE: " + checkRelation(boxD, boxE) + " - " + test(checkRelation(boxD, boxE), boxRelation.separate));         // separate is the default for a box in a box
@@ -26,5 +28,3 @@ console.log("boxD and boxE not assuming a box in a box is separate: " + checkRel
 console.log("a separate array created from arr not assuming a box in a box is separate:");
 console.log(checkArray(arr, boxRelation.inside));
 console.log("the array's length is " + (checkArray(arr, boxRelation.inside).length <= 3 ? " <= 3 - " + passed : " > 3 - " + failed));
-const boxF: BoundingBox = {left: 40, right: 30, top: 30, bottom: 10};
-console.log("boxA and boxF: " + checkRelation(boxA, boxF) + " - " + test(checkRelation(boxA, boxF), boxRelation.invalidHorizontal));
