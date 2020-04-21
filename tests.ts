df016357f18b0968d4ba509de58e74580e5360a4
oz <ozel@post.bgu.ac.il>
2020-04-21 15:38:33 +0300

    a slight change to the tests

--- tests.ts
+++ tests.ts
@@ -2,10 +2,24 @@ import { BoundingBox, checkRelation, checkArray, boxRelation, pointsToBox } from
 
 const passed: string = "Test passed";
 const failed: string = "Test failed"
+let totalTests: number = 0;
+let passedTests: number = 0;
 
 const test = (str1: string, str2: string) : string => {
-    if (str1.slice(0, str2.length) === (str2))
+    totalTests++;
+    if (str1.slice(0, str2.length) === (str2)) {
+        passedTests++;
         return passed;
+    }
+    return failed;
+}
+
+const testLen = (arr: BoundingBox[], n: number) : string => {
+    totalTests++;
+    if (arr.length <= n){
+        passedTests++;
+        return passed;
+    }
     return failed;
 }
 
@@ -24,9 +38,10 @@ console.log("boxA and boxC: " + checkRelation(boxA, boxC) + " - " + test(checkRe
 console.log("boxD and boxE: " + checkRelation(boxD, boxE) + " - " + test(checkRelation(boxD, boxE), boxRelation.separate));         // separate is the default for a box in a box
 console.log("a separate array created from arr:");
 console.log(checkArray(arr));
-console.log("the array's length is " + (checkArray(arr).length <= 4 ? " <= 4 - " + passed : " > 4 - " + failed));
+console.log("the array's length is " + (testLen(checkArray(arr), 4) === passed ? " <= 4 - " + passed : " > 4 - " + failed));
 console.log("boxD and boxE not assuming a box in a box is separate: " + checkRelation(boxD, boxE, boxRelation.inside) + " - " + test(checkRelation(boxD, boxE, boxRelation.inside), boxRelation.inside));
 console.log("a separate array created from arr not assuming a box in a box is separate:");
 console.log(checkArray(arr, boxRelation.inside));
-console.log("the array's length is " + (checkArray(arr, boxRelation.inside).length <= 3 ? " <= 3 - " + passed : " > 3 - " + failed));
+console.log("the array's length is " + (testLen(checkArray(arr, boxRelation.inside),3) === passed ? " <= 3 - " + passed : " > 3 - " + failed));
 console.log("comparing boxA created using four edges to boxA created using two points - " + test(JSON.stringify(boxA), JSON.stringify(boxAbyPoints)));
+console.log("passed " + passedTests + "/" + totalTests + " tests");
